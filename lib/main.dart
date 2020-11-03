7aab6e51c716c4dcb1c7cc9f5d950279b09b39b0
Augustine Victor <augustinevickky@gmail.com>
2020-11-03 19:16:34 +0100

    update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -10,13 +10,16 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+    return ChangeNotifierProvider(
+      create: (context) => UiSettings(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: ProviderDemo(),
       ),
-      home: ProviderDemo(),
     );
   }
 }
